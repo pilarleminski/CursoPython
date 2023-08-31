1675467c11674f42341b0fc41cd37e83b87bd4ee
Pilar Leminski Veiga <pilar.leminski@gmail.com>
2023-08-30 21:07:27 -0300

    deploy

--- exercicio04.py
+++ exercicio04.py
@@ -1,15 +1,15 @@
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
-nome_invertido = nome[len(nome)::-1]
 
-if len(nome) and len(idade):
+if nome and idade:
     print(f'Seu nome é {nome}')
-    print(f'Seu nome invertido é {nome_invertido}')
+    print(f'Seu nome invertido é {nome[::-1]}')
     if ' ' in nome:
         print(f'Seu nome contém espaços')
     else:
         print('Seu nome não contém espaços')
     print(f'Seu nome contém {len(nome)} letras')
-    print(f'A última letra do seu nome é {nome[-1]}')
+    print(f'A primeira letra do seu nome é "{nome[0]}"')
+    print(f'A última letra do seu nome é "{nome[-1]}"')
 else:
     print('Desculpe, você deixou campos vazios')
