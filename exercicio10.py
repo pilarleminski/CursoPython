2c99e24e3853d7999099beb9459f3ff3b51806af
Pilar Leminski Veiga <pilar.leminski@gmail.com>
2023-09-01 21:04:26 -0300

    deploy

---
+++ exercicio10.py
@@ -0,0 +1,18 @@
+frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por guido van Rossum.'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+while i < len(frase):
+    letra_atual = frase[i]
+    i += 1
+    if letra_atual == ' ':
+        continue
+
+    qtd_atual = frase.count(letra_atual)
+    if qtd_apareceu_mais_vezes < qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+    
+
+print(f'A letra que apareceu mais vezes foi {letra_apareceu_mais_vezes}, que apareceu {qtd_apareceu_mais_vezes}x')
